ed18cc83e756c1b12997e4e67a859d98f30057e1
Will Smith <yokoref@gmail.com>
2015-10-05 01:49:46 -0500

    Initial Commit

---
+++ TestToGitRespository/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TestToGitRespository
+//
+//  Created by William Smith on 10/5/15.
+//  Copyright © 2015 William Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TestToGitRespository/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestToGitRespository
+//
+//  Created by William Smith on 10/5/15.
+//  Copyright © 2015 William Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
